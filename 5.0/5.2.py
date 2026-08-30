6c6927441e6a26e444a53d74333d9ec7de70495f
1UG4 <rufusb995@gmail.com>
2026-08-30 16:31:26 +0300

    mmmmm

---
+++ 5.0/5.2.py
@@ -0,0 +1,12 @@
+numero_lista = []
+
+while True:
+    kysymys = input("Luku: ")
+
+    if kysymys != "":
+        numero_lista.append(kysymys)
+
+    if kysymys == "":
+        numero_lista.sort(reverse=True)
+        print(f"{numero_lista}")
+        break
