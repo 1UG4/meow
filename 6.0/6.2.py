cea0863f042d881b3bd78fd690fcda6973b940bf
1UG4 <rufusb995@gmail.com>
2026-09-03 15:58:44 +0300

    mmmmm

---
+++ 6.0/6.2.py
@@ -0,0 +1,15 @@
+import random
+
+tahkojen_maara = int(input("Syötä tahkojen määrä: "))
+
+def satunnainen_silmaluku(tahkojen_maara):
+    return random.randint(1,tahkojen_maara)
+
+
+while True:
+    tulos = satunnainen_silmaluku(tahkojen_maara)
+
+    print(f"Silmäluku: {tulos}")
+
+    if tulos == tahkojen_maara:
+        break
---
+++ 6.0/6.3.py
@@ -0,0 +1,12 @@
+def nestegallat_litroiksi(x):
+    lasku = x * 3.785
+
+    return lasku
+
+while True:
+    x = float(input("Bensiinin määrä Yhdysvaltain nestegallonoina: "))
+
+    if x < 0:
+        break
+
+    print(f" Litraa: {nestegallat_litroiksi(x)}")
